b0766b6b3e640259ff7a9dbaa188cb9dfe9cbbb7
alphino <44781856+alphino@users.noreply.github.com>
2021-12-30 00:43:08 -0800

    final

--- src/client/server/login-signin.php
+++ src/client/server/login-signin.php
@@ -1,37 +1,61 @@
-<?php  
-$username = $_POST["username"];
-$password = md5($_POST["password"]);
-if ($_SERVER['REQUEST_METHOD'] == "POST") {
-    $host = "localhost";
-    $database = "gamex";
-    $user = "webuser";
-    $password = "P@ssw0rd";
+<!DOCTYPE html>
+<html>
+<body>
+<?php
 
-    $connection = mysqli_connect($host, $user, $password, $database);
-    $error = mysqli_connect_error();
-    if ($error != null) {
-        $output = "<p>Unable to connect to database!</p>";
-        exit($output);
-    } else {
-        $sql = $connection->prepare("SELECT username FROM users WHERE username = '$username' and password = '$password'");
-        $sql->execute();
-        $complete = $sql->get_result();
-        $entered = $complete->fetch_assoc();
+$host     = "localhost";
+$database = "gamex";
+$user     = "webuser";
+$password = "P@ssw0rd";
 
-        while ($entered){
+$connection = mysqli_connect($host, $user, $password, $database);
+
+$error = mysqli_connect_error();
+if($error != null)
+{
+  $output = "<p>Unable to connect to database!</p>";
+  exit($output);
+}
+else
+{
+
+    if (isset($_SERVER["REQUEST_METHOD"]) &&  $_SERVER["REQUEST_METHOD"] == "POST")
+    {
+      if (isset($_POST["username"]))
+
+        $user_name = $_POST["username"];
+      if (isset($_POST["password"]))
+
+        $password1 = $_POST["password"];
+
+        $sql = "SELECT * FROM users where username = '$user_name' AND password = '$password1';";
+
+        $results = mysqli_query($connection, $sql);
+
+        if ($row = mysqli_fetch_assoc($results))
+        {
             echo "WELCOME " . $entered['username'] . "!";
             header("Location: ../html/main1.html");
-            mysqli_free_result($complete);
-            mysqli_close($connection);
-            die; 
         }
-        echo "wrong username or password entered";
-        mysqli_free_result($complete);
-        mysqli_close($connection);
-        die;
+        else
+        {
+          echo "<p>Invalid username and/or password </p>";
+          if (isset($return_link))
+          {
+            echo '<a href="'.$return_link.'">Return to user entry</a>';
+          }
+        }
+        mysqli_free_result($results);
+
     }
-} 
-if ($_SERVER['REQUEST_METHOD'] == "GET") {
-    die("Unable to get data!");
+    else {
+
+      echo "<p>Bad login</p>";
+
+    }
+
+    mysqli_close($connection);
 }
 ?>
+</body>
+</html>
